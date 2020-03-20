fcc96c1391d9c9a7578bb0931c85e4377c6a6f6c
Jack <jack@nustechnology.com>
2020-03-20 14:36:42 +0700

    fix delayed_job

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/var/www/rental"
 # Default value for :linked_files is []
 set :linked_files, %w(config/database.yml config/config.yml)
 # Default value for linked_dirs is []
-set :linked_dirs, %w(log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads .bundle)
+set :linked_dirs, %w(log bin/delayed_job tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads .bundle)
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
