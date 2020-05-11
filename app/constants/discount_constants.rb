59b658179dde290a8607e78fab48e52881348bea
Jack <jack@nustechnology.com>
2020-05-11 09:28:21 +0700

    Add price calculation and add default days for listing is 7 days

---
+++ app/constants/discount_constants.rb
@@ -0,0 +1,9 @@
+module DiscountConstants
+  TWO_DAYS   = 45.45
+  THREE_DAYS = 61.04
+  FOUR_DAYS  = 68.51
+  FIVE_DAYS  = 71.95
+  SIX_DAYS   = 74.46
+  SEVEN_DAYS = 76.07
+  EIGHT_DAYS = 76.62
+end
---
+++ app/services/price_calculation_service.rb
@@ -0,0 +1,33 @@
+module PriceCalculationService
+  module_function
+
+  def calculate(listing, days = 7)
+    a_day_price = listing.price.cents
+
+    day_price = a_day_price - (a_day_price * PriceCalculationService.get_discount_percent(days)/100).round
+
+    (day_price * days)
+  end
+
+  def get_discount_percent(days)
+    case days
+    when 2
+      DiscountConstants::TWO_DAYS
+    when 3
+      DiscountConstants::THREE_DAYS
+    when 4
+      DiscountConstants::FOUR_DAYS
+    when 5
+      DiscountConstants::FIVE_DAYS
+    when 6
+      DiscountConstants::SIX_DAYS
+    when 7
+      DiscountConstants::SEVEN_DAYS
+    when 8
+      DiscountConstants::EIGHT_DAYS
+    else
+      DiscountConstants::SEVEN_DAYS
+    end
+  end
+
+end
