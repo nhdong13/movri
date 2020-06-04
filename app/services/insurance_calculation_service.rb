3dcfb3fd9c53a3981b337f6f657ca6b35f60f7fe
Jack <jack@nustechnology.com>
2020-06-04 17:26:27 +0700

    change persent of replacement fee

--- app/services/insurance_calculation_service.rb
+++ app/services/insurance_calculation_service.rb
@@ -9,7 +9,7 @@ module InsuranceCalculationService
     price_dates = booking_dates + 2
 
     # 0.1% of listing replacement_value
-    insurance_price_per_day = 0.1 * listing.replacement_cents_fee * price_dates
+    insurance_price_per_day = 0.001 * listing.replacement_cents_fee * price_dates
 
     return MIN_INSURANCE_PRICE unless MIN_INSURANCE_PRICE < insurance_price_per_day
 
