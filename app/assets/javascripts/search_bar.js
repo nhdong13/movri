1f4773f624dcad6ea4f0f2412fd05177c59336d6
Jack <jack@nustechnology.com>
2020-08-13 15:06:39 +0700

    fix es6 syntag error

--- app/assets/javascripts/search_bar.js
+++ app/assets/javascripts/search_bar.js
@@ -403,7 +403,7 @@ $(document).ready(function() {
         ${items.map(renderListItem).join('')}
       </div>
     `;
-    [...widgetParams.container.querySelectorAll('button')].forEach(element => {
+    [widgetParams.container.querySelectorAll('button')][0].forEach(element => {
       element.addEventListener('click', event => {
         const item = Object.keys(event.currentTarget.dataset).reduce(
           (acc, key) => ({
