bd87b0d9777ff414f914338cd401f7c3cd1e8197
Jack <jack@nustechnology.com>
2020-09-16 15:21:08 +0700

    update tabs UI

--- app/models/listing_tab.rb
+++ app/models/listing_tab.rb
@@ -17,4 +17,19 @@ class ListingTab < ApplicationRecord
   TAB_TYPES = ["specs", "overview", "q_and_a", "in_the_box", "not_in_the_box", "key_features"]
   belongs_to :listing
   scope :extra_tabs, -> { where.not(tab_type: TAB_TYPES) }
+
+  def get_icon
+    case tab_type
+    when 'specs'
+      "fa-file"
+    when 'overview'
+      'fa-eye'
+    when 'q_and_a'
+      'fa-question-circle'
+    when 'in_the_box'
+      'fa-archive'
+    else
+      'fa-plus'
+    end
+  end
 end
