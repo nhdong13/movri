097173ea1c8f56e98f2f0d165bcbb621de501d5e
Jack <jack@nustechnology.com>
2020-09-23 13:39:24 +0700

    fix bug when mising packing dimension

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -391,6 +391,7 @@ class Transaction < ApplicationRecord
     weight = 0
     transaction_items.each do |item|
       packing_dimension = item.listing.packing_dimensions.first
+      return true unless packing_dimension.weight
       weight += (packing_dimension.weight) * item.quantity
     end
     weight > LIMIT_WEIGHT_OF_FEDEX_SERVICE
