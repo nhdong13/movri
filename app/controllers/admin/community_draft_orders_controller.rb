51908d8129b1a9c4cabdf195d391f8af0ccfbcc8
Jack <jack@nustechnology.com>
2020-12-10 14:01:55 +0700

    update edit draft order

--- app/controllers/admin/community_draft_orders_controller.rb
+++ app/controllers/admin/community_draft_orders_controller.rb
@@ -12,18 +12,11 @@ class Admin::CommunityDraftOrdersController < Admin::AdminBaseController
   end
 
   def edit
-    rate_type = PriceCalculationService.get_rate_type_of_canada_provinces(@order.shipping_address&.state_or_province)
-
-    # @payment_info = {
-    #   GST: rate_type[:GST],
-    #   PST: rate_type.key?(:pst) ? rate_type[:PST] : 0,
-    #   total: rate_type.key?(:pst) ? @order.total_price_cents + (@order.total_price_cents * rate_type[:GST] / 100) + (@order.total_price_cents * rate_type[:PST] / 100) : @order.total_price_cents + (@order.total_price_cents * rate_type[:GST] / 100)
-    # }
   end
 
   def update
-    if @order.update_attributes(transaction_params)
-      redirect_to action: 'edit', id: @order.id
+    if @transaction.update_attributes(transaction_params)
+      redirect_to action: 'edit', id: @transaction.id
     end
   end
 
@@ -40,7 +33,7 @@ class Admin::CommunityDraftOrdersController < Admin::AdminBaseController
   end
 
   def set_order
-    @order = Transaction.find(params[:id])
+    @transaction = Transaction.find(params[:id])
   end
 
   def transaction_params
