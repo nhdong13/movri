708b51b7d440ffac2ee1ca9cd3de8c686f402e49
Jack <jack@nustechnology.com>
2021-07-30 00:23:03 +0700

    Add eta for fedex express saver

--- app/helpers/transaction_addresses_helper.rb
+++ app/helpers/transaction_addresses_helper.rb
@@ -2,6 +2,7 @@ module TransactionAddressesHelper
   def shipping_type_to_humanized shipping_type
     case shipping_type
     when "fedex_ground"
+    when "fedex_express_saver"
       "3 business days"
     when "fedex_standard_overnight"
       "1 business day"
